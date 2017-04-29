8311584a7e7ca129a2f1b025ce1fae7ac9bb32f1
Vladimir Novikov <novavovikov@gmail.com>
2017-04-29 16:18:44 +0300

    experiments with classes

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -13,98 +13,122 @@ import { setPositionItem } from './actions/setPositionItem';
 
 import { findItemsHandle } from './selectors/selectors';
 
-import Wrap from './components';
+import Input from './components/input';
+import List from './components/list';
+import Notice from './components/notice';
+import Message from './components/message';
 
 import './autocomplete.css';
 
-//
-const AutoComplete = ({list, findItems, showList, findText, selectItem, itemPosition, onGetList, onToggleList, onFindText, onFindList, onSelectItem, showLoader, setPositionItem, message, setMessage, notice, setNotice}) => {
-	let newItemPosition = itemPosition;
 
-	const changeInput = (item) => {
-		if (list.length === 0) {
-			onGetList()
+class AutoComplete extends Component {
+	Constructor() {
+		this.newItemPosition = 0;
+	}
+
+
+	changeInput(item) {
+		if (this.props.list.length === 0) {
+			this.props.onGetList()
 		}
 
-		newItemPosition = 0;
+		this.newItemPosition = 0;
 
-		if (findItems.length > 0) {
-			onSelectItem(findItems[newItemPosition].Id)
+		if (this.props.findItems.length > 0) {
+			this.props.onSelectItem(this.props.findItems[this.newItemPosition].Id)
 		};
 
-		onFindText(item.target.value);
+		this.props.onFindText(item.target.value);
 
-		let findList = findItemsHandle(list, item.target.value);
+		let findList = findItemsHandle(this.props.list, item.target.value);
 
-		onFindList(findList);
-		onToggleList(true);
+		this.props.onFindList(findList);
+		this.props.onToggleList(true);
 	}
 
-	const keyUpHandle = (e) => {
-		(findItems.length === 0) ? setMessage('Не найдено') : setMessage('');
+	keyUpHandle(e) {
+		(this.props.findItems.length === 0) ? setMessage('Не найдено') : setMessage('');
 	}
 
-	const blurHandle = () => {
-		if (findItems.length !== 0) {
-			onSelectItem(findItems[0].Id);
-			onFindText(findItems[0].City);
+	blurHandle() {
+		if (this.props.findItems.length !== 0) {
+			this.props.onSelectItem(this.props.findItems[0].Id);
+			this.props.onFindText(this.props.findItems[0].City);
 		} else {
 			setMessage('')
 			setNotice('Выберите значание из списка')
 		}
 
-		onToggleList(false);
+		this.props.onToggleList(false);
 	}
 
-	const keyDownHandle = (e) => {
+	keyDownHandle(e) {
 		if (e.key === 'ArrowDown') {
-			newItemPosition = itemPosition + 1;
+			this.newItemPosition = this.props.itemPosition + 1;
 		} else if (e.key === 'ArrowUp') {
-			newItemPosition = itemPosition - 1;
+			this.newItemPosition = this.props.itemPosition - 1;
 		} else if (e.key === 'Escape') {
-			onToggleList(false);
-		} else if (e.key === 'Enter' && findItems.length > 0) {
-			onSelectItem(findItems[itemPosition].Id);
-			onFindText(findItems[itemPosition].City);
-			onToggleList(false);
+			this.props.onToggleList(false);
+		} else if (e.key === 'Enter' && this.props.findItems.length > 0) {
+			this.props.onSelectItem(this.props.findItems[this.props.itemPosition].Id);
+			this.props.onFindText(this.props.findItems[this.props.itemPosition].City);
+			this.props.onToggleList(false);
 		}
 
-		if (newItemPosition < 0) {
+		if (this.newItemPosition < 0) {
 			setPositionItem(0);
-			newItemPosition = 0;
-		} else if (newItemPosition > findItems.length - 1) {
-			setPositionItem(findItems.length - 1);
-			newItemPosition = findItems.length - 1;
+			this.newItemPosition = 0;
+		} else if (this.newItemPosition > this.props.findItems.length - 1) {
+			setPositionItem(this.props.findItems.length - 1);
+			this.newItemPosition = this.props.findItems.length - 1;
 		} else {
-			setPositionItem(newItemPosition);
+			this.props.setPositionItem(this.newItemPosition);
 		}
 
-		if (findItems.length > 0 && showList === true) {
-			onSelectItem(findItems[newItemPosition].Id)
+		if (this.props.findItems.length > 0 && this.props.showList === true) {
+			this.props.onSelectItem(this.props.findItems[this.newItemPosition].Id)
 		};
 	}
 
-	return (
-		<Wrap 
-			showLoader={showLoader}
-			findText={findText}
-			changeInput={changeInput}
-			keyUpHandle={keyUpHandle}
-			blurHandle={blurHandle}
-			keyDownHandle={keyDownHandle}
-			setNotice={setNotice}
-			message={message}
-			notice={notice}
-			findItems={findItems}
-			showList={showList}
-			selectItem={selectItem}
-			onSelectItem={onSelectItem}
-			onFindText={onFindText}
-			onToggleList={onToggleList}
-		/>
-	)
-}
 
+	render() {
+		return (
+			<div className='autocomplete'>
+				{(this.props.showLoader) ? (
+					<div className="autocomplete__loader" />
+				) : ''}
+
+				<Input 
+					findText={this.props.findText}
+					changeInput={this.changeInput.bind(this)}
+					keyUpHandle={this.keyUpHandle.bind(this)}
+					blurHandle={this.blurHandle.bind(this)}
+					keyDownHandle={this.keyDownHandle.bind(this)}
+					setNotice={this.props.setNotice} 
+				/>
+
+				{(this.props.message !== '') ? (
+					<Message message={this.props.message} />
+				) : ''}
+
+				{(this.props.notice !== '') ? (
+					<Notice notice={this.props.notice} />
+				) : ''}
+
+				{(this.props.findItems.length !== 0) ? (
+					<List 
+						findItems={this.props.findItems}
+						showList={this.props.showList}
+						selectItem={this.props.selectItem}
+						onSelectItem={this.props.onSelectItem}
+						onFindText={this.props.onFindText}
+						onToggleList={this.props.onToggleList}
+					/>
+				) : ''}
+			</div>
+		)
+	}
+}
 
 //
 function mapStateToProps(state) {
